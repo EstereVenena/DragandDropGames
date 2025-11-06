6f9d0edb5f44bd1b0daf05722d65dbd04eb38a85
EstereVenena <venena.estere@gmail.com>
2025-11-06 14:24:38 +0200

    Started on adds

---
+++ Assets/Scripts/AddManeger.cs
@@ -0,0 +1,49 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class AddManeger : MonoBehaviour
+{
+    public AdsInirializer adsInitializer;
+    public 
+ private void Awake()
+    {
+        if (adsInitializer == null)
+            adsInitializer = FindFirstObjectByType<AdsInirializer>();
+        if instance != null && ) 
+        { }
+    }
+    
+    {
+        )
+    }
+
+
+private void HandleAdsInitiakized()
+    if (!IsUdtReturn of )
+}
+
+
+
+
+
+
+    public void ShowAd()
+{
+    if (isReady)
+    {
+        //... baner reklamas
+    }
+}
+
+        ShowAd()
+
+        }else debuglog(Inrwestistal ad not ready)
+
+        debug log("Showing ad now ")
+    time.timescale = 0f;
+interstitialAdbutton = Button
+    intersttitialAdbutton.iteractible = false
+    }
+    }
---
+++ Assets/Scripts/AdsInirializer.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInirializer : MonoBehaviour
+{
+    [Sterilizefield] string _adnroidGAmeId
+        [Sterilizefield] bool _testMode = true
+        private string _gameID
+        public event Action OnAdsInitilized
+}
+#endif
+
+if(!Advertisement.isInitilized &&Advertisement.isSupported)
+    Advertisement.isInitialize()
+
+        if ad maneger 
+        debug.Logwarning("Tried to liad an ad before unity ads where initilieds")
